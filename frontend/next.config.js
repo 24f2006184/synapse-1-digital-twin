6e18f912d38c81a836a78bfba4d7b2461ae51f2f
Aparna-Jha2004 <aparnajha012@gmail.com>
2026-04-15 11:53:10 +0530

    Add webpack aliases for frontend path resolution on Render

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,7 +1,17 @@
 /** @type {import('next').NextConfig} */
+const path = require("path");
+
 const nextConfig = {
   reactStrictMode: false,
   transpilePackages: ["three", "@react-three/fiber", "@react-three/drei"],
+  webpack(config) {
+    config.resolve.alias = {
+      ...(config.resolve.alias || {}),
+      "@/lib": path.resolve(__dirname, "./lib"),
+      "@/components": path.resolve(__dirname, "./components"),
+    };
+    return config;
+  },
 };
 
 module.exports = nextConfig;
